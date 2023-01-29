37c81ead00ce2f78d70b40d38d6d48566afe56b6
toum <nanhtavadh@gmail.com>
2023-01-29 18:55:20 +0700

    update style

--- lib/intl_phone_form_field.dart
+++ lib/intl_phone_form_field.dart
@@ -7,72 +7,82 @@ import 'package:intl_phone_form_field/src/utils/phone_number.dart';
 import 'src/intl_form_country.dart';
 import 'src/utils/countries.dart';
 
-class IntlPhoneFormField extends FormBuilderField<PhoneNumber> {
+class IntlPhoneFormField extends FormBuilderField<String> {
   final String? initalCountryCode;
-  final PhoneNumber? initalPhoneNumber;
   final String? selectTitle;
   final TextStyle? selectTitleStyle;
-
+  final bool showFlag;
+  final TextEditingController? controller;
   IntlPhoneFormField({
     Key? key,
     required String name,
+    InputDecoration decoration = const InputDecoration(),
+    String? initialValue,
+    FormFieldValidator<String>? validator,
     this.initalCountryCode,
-    this.initalPhoneNumber,
     this.selectTitle,
     this.selectTitleStyle,
+    this.showFlag = false,
+    this.controller,
   }) : super(
             key: key,
             name: name,
-            initialValue: initalPhoneNumber,
-            builder: (FormFieldState<PhoneNumber> field) {
+            initialValue: controller?.text,
+            validator: validator,
+            builder: (FormFieldState<String?> field) {
               final state = field as _IntlPhoneFormFieldState;
               return TextField(
-                decoration: state.decoration.copyWith(
-                    prefixIcon: state._buildFlagButton(),
-                    border: OutlineInputBorder()),
+                controller: state._effectiveController,
+                decoration: decoration.copyWith(
+                  prefixIcon: state._buildFlagButton(),
+                ),
               );
             });
+
+
+  
   @override
-  FormBuilderFieldState<FormBuilderField<PhoneNumber>, PhoneNumber>
+  FormBuilderFieldState<FormBuilderField<String>, String>
       createState() => _IntlPhoneFormFieldState();
 }
 
 class _IntlPhoneFormFieldState
-    extends FormBuilderFieldState<IntlPhoneFormField, PhoneNumber> {
+    extends FormBuilderFieldState<IntlPhoneFormField, String> {
   late Country _selectedCountry;
   late List<Country> _countryList;
   late String _initalCountryCode;
-  late PhoneNumber _phoneNumber;
+  TextEditingController? get _effectiveController => widget.controller??_controller;
+  TextEditingController? _controller;
+  
+
   @override
   void initState() {
     super.initState();
+    _controller = widget.controller ?? TextEditingController(text: value);
+    _controller?.addListener(_handleControllerChanged);
     _countryList = countries;
-    _initalCountryCode = widget.initalCountryCode ?? "US";
+    _initalCountryCode = widget.initalCountryCode ?? "1";
     _selectedCountry = _countryList.firstWhere(
         (country) =>
-            _initalCountryCode.toLowerCase() == country.code.toLowerCase(),
+            _initalCountryCode.toLowerCase() == country.dialCode.toLowerCase(),
         orElse: () => _countryList.first);
-
-    //Phone number
-    _phoneNumber = PhoneNumber(
-      countryCode: _selectedCountry.dialCode,
-      countryISOCode: _selectedCountry.code,
-      number: "",
-    );
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    _controller?.removeListener(_handleControllerChanged);
+    if(null == widget.controller){
+      _controller?.dispose();
+    }
     super.dispose();
   }
-
   @override
-  void didChange(PhoneNumber? value) {
-    // TODO: implement didChange
-    super.didChange(value);
+  void didChange(String? value) {
+    super.didChange("${_selectedCountry.dialCode}$value");
+    if(_effectiveController?.text != value){
+      _effectiveController?.text = value??"";
+    }
   }
-
   Widget _buildFlagButton() {
     return Container(
       margin: const EdgeInsets.only(right: 5),
@@ -81,6 +91,15 @@ class _IntlPhoneFormFieldState
           mainAxisSize: MainAxisSize.min,
           children: [
             const Icon(Icons.arrow_drop_down),
+            if (widget.showFlag) ...[
+              Image.asset(
+                'assets/flags/${_selectedCountry.code.toLowerCase()}.png',
+                package: 'intl_phone_form_field',
+                width: 30,
+                height: 20,
+              ),
+              const SizedBox(width: 8),
+            ],
             Text("+${_selectedCountry.dialCode}"),
           ],
         ),
@@ -96,11 +115,24 @@ class _IntlPhoneFormFieldState
           if (country != null) {
             setState(() {
               _selectedCountry = country;
-              // didChange(_phoneNumber);
+              didChange(_effectiveController?.text);
             });
           }
         },
       ),
     );
+  }
+
+  void _handleControllerChanged() {
+    // Suppress changes that originated from within this class.
+    //
+    // In the case where a controller has been passed in to this widget, we
+    // register this change listener. In these cases, we'll also receive change
+    // notifications for changes originating from within this class -- for
+    // example, the reset() method. In such cases, the FormField value will
+    // already have been set.
+    if (_effectiveController!.text != (value ?? '')) {
+      didChange(_effectiveController?.text);
+    }
   }
 }
--- lib/src/intl_form_country.dart
+++ lib/src/intl_form_country.dart
@@ -2,22 +2,71 @@ import 'package:flutter/material.dart';
 
 import 'utils/countries.dart';
 
-class IntlFormContry extends StatelessWidget {
+class IntlFormContry extends StatefulWidget {
   final String? title;
   final TextStyle? titelTextStyle;
+  final String hintText;
   const IntlFormContry({
     super.key,
     this.title,
     this.titelTextStyle,
+    this.hintText = "Search",
   });
+
+  @override
+  State<IntlFormContry> createState() => _IntlFormContryState();
+}
+
+class _IntlFormContryState extends State<IntlFormContry> {
+  late TextEditingController _controller;
+  late List<Country> _countriesList;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _countriesList = countries;
+    _controller.addListener(() {
+      searchCountry();
+    });
+  }
+
+  void searchCountry() {
+    setState(() {
+      if (_controller.text.isEmpty) {
+        _countriesList = countries;
+      } else {
+        _countriesList = countries
+            .where(
+              (country) =>
+                  country.name
+                      .toLowerCase()
+                      .contains(_controller.text.toLowerCase()) ||
+                  country.code
+                      .toLowerCase()
+                      .contains(_controller.text.toLowerCase()) ||
+                  country.dialCode
+                      .toLowerCase()
+                      .contains(_controller.text.toLowerCase()),
+            )
+            .toList();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          title ?? '',
-          style: titelTextStyle,
+          widget.title ?? '',
+          style: widget.titelTextStyle,
         ),
       ),
       body: Padding(
@@ -25,22 +74,41 @@ class IntlFormContry extends StatelessWidget {
         child: Column(
           children: [
             TextField(
-              decoration: InputDecoration(hintText: "Search"),
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: widget.hintText,
+                suffixIcon:const Icon(Icons.search),
+              ),
             ),
             const SizedBox(height: 10),
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
-                  children: countries
-                      .map((country) => ListTile(
-                            leading: Text(country.flag),
-                            title: Text(country.name),
-                            trailing: Text("+${country.dialCode}"),
-                            onTap: () {
-                              Navigator.of(context).pop<Country>(country);
-                            },
-                          ))
-                      .toList(),
+                  children: _countriesList.map((country) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          contentPadding: EdgeInsets.zero,
+                          minVerticalPadding: 8,
+                          dense: true,
+                          leading: Text(
+                            country.flag,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium
+                                ?.copyWith(color: Colors.black),
+                          ),
+                          title: Text(country.name,
+                              style: Theme.of(context).textTheme.bodyLarge),
+                          trailing: Text("+${country.dialCode}"),
+                          onTap: () {
+                            Navigator.of(context).pop<Country>(country);
+                          },
+                        ),
+                        const Divider(),
+                      ],
+                    );
+                  }).toList(),
                 ),
               ),
             )
